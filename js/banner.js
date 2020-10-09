52eb91560ca9e974f45f6798c3d98c9dc949ef0b
Rodrigo P.A <rodrigoidaraujo@gmail.com>
2020-10-09 14:41:27 -0300

    att js

--- js/banner.js
+++ js/banner.js
@@ -1,1 +1,27 @@
-!function(e){var o=["Seja Bem-vindo ao meu site.","Me chamo Rodrigo, sou Desenvolvedor Front-End e Web Design Jr.","Sou estudante de Análise e desenvolvimento de sistemas da Unopar.","Sou estagiário de desenvolvimento web na Medilab Sistemas.","Meu objetivo é crescer e desenvolver profissionalmente junto à empresa."],n=0,t=0,s="";!function i(){n!==o.length&&(e("#textTyping").html(s+o[n].substring(0,t)+"_"),t+=1,o[n].length+1===t?(s+=o[n]+"<br />",n+=1,t=0,setTimeout(()=>{i()},50)):setTimeout(()=>{i()},50))}()}(jQuery);
+!(function (e) {
+  var o = [
+      "Seja Bem-vindo ao meu site.",
+      "Me chamo Rodrigo, sou Desenvolvedor Front-End e Web Design Jr.",
+      "Sou estudante de Análise e desenvolvimento de sistemas da Unopar.",
+      "Buscando estágio em desenvolvedor front end react",
+      "Meu objetivo é crescer e desenvolver profissionalmente junto à empresa.",
+    ],
+    n = 0,
+    t = 0,
+    s = "";
+  !(function i() {
+    n !== o.length &&
+      (e("#textTyping").html(s + o[n].substring(0, t) + "_"),
+      (t += 1),
+      o[n].length + 1 === t
+        ? ((s += o[n] + "<br />"),
+          (n += 1),
+          (t = 0),
+          setTimeout(() => {
+            i();
+          }, 50))
+        : setTimeout(() => {
+            i();
+          }, 50));
+  })();
+})(jQuery);
